f8ee7d878ae0707385b90eafc6b9d4913dc99edf
Pedro Esmerio <p.h.esmerio@gmail.com>
2022-04-25 11:08:20 -0300

    New configuration for config.lua

--- lvim/config.lua
+++ lvim/config.lua
@@ -13,7 +13,8 @@ lvim.log.level = "warn"
 lvim.format_on_save = true
 lvim.colorscheme = "dracula"
 lvim.transparent_window = true
-
+lvim.format_on_save = true
+vim.opt.relativenumber = true
 
 -- keymappings [view all the defaults by pressing <leader>Lk]
 lvim.leader = "space"
@@ -27,6 +28,11 @@ lvim.keys.normal_mode["<C-s>"] = ":w<cr>"
 -- edit a default keymapping
 lvim.keys.normal_mode["<C-q>"] = ":q<cr>"
 
+-- Adding one line to load the LazyGit window
+lvim.builtin.which_key.mappings["g"] = {
+  name = "Git",
+  g = {"<cmd>LazyGit<cr>", "Load LazyGit"},
+}
 
 -- Change Telescope navigation to use j and k for navigation and n and p for history in both input and normal mode.
 -- we use protected-mode (pcall) just in case the plugin wasn't loaded yet.
@@ -46,51 +52,81 @@ lvim.builtin.telescope.defaults.mappings = {
   },
 }
 
+lvim.builtin.lualine.options.theme = "dracula"
+lvim.builtin.lualine.options.section_separators = { left = "", right = "" }
+lvim.builtin.lualine.sections.lualine_a = {"mode"}
+
 -- Use which-key to add extra bindings with the leader-key prefix
 lvim.builtin.which_key.mappings["P"] = { "<cmd>Telescope projects<CR>", "Projects" }
-lvim.builtin.which_key.mappings["x"] = { "<cmd>BufferKill<CR>", "Close Buffer" }
-
--- Adding one line to load the LazyGit window
-lvim.builtin.which_key.mappings["g"] = {
-  name = "Git",
-  g = {"<cmd>LazyGit<cr>", "Load LazyGit"},
-  j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-  k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-  l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-  p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-  r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-  R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-  s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-  u = {
-    "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-    "Undo Stage Hunk",
-  },
-  o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
-  b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
-  c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
-  C = {
-    "<cmd>Telescope git_bcommits<cr>",
-    "Checkout commit(for current file)",
-  },
-  d = {
-    "<cmd>Gitsigns diffthis HEAD<cr>",
-    "Git Diff",
-  },
+lvim.builtin.which_key.mappings["t"] = {
+  name = "+Trouble",
+  r = { "<cmd>Trouble lsp_references<cr>", "References" },
+  f = { "<cmd>Trouble lsp_definitions<cr>", "Definitions" },
+  d = { "<cmd>Trouble document_diagnostics<cr>", "Diagnostics" },
+  q = { "<cmd>Trouble quickfix<cr>", "QuickFix" },
+  l = { "<cmd>Trouble loclist<cr>", "LocationList" },
+  w = { "<cmd>Trouble workspace_diagnostics<cr>", "Wordspace Diagnostics" },
 }
 
 -- TODO: User Config for predefined plugins
 -- After changing plugin config exit and reopen LunarVim, Run :PackerInstall :PackerCompile
-lvim.builtin.dashboard.active = true
+
 lvim.builtin.notify.active = true
 lvim.builtin.terminal.active = true
+
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.show_icons.git = 0
+
+-- dashboard configuration for alpha plugin
+lvim.builtin.alpha.active = true
+lvim.builtin.alpha.mode = "dashboard"
+lvim.builtin.alpha.dashboard.section.header.val = {
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                           ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣼⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡟⠀⠀⠀⠀⠀⠀⠀⠀⣰⠟⠁⡈⢣⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣷⡄⠀⠀⠀⠀⠀⢀⡼⠋⠀⠀⢰⡌⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣦⡀⠀⠀⣠⡟⠁⠀⣠⣤⣼⣷⣿⡄⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣿⣿⣿⣿⣦⣴⡟⠀⢸⣾⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⣿⣿⣿⣿⣿⡿⠿⠀⢸⣿⣿⣿⣿⣿⣿⣿⡀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⠶⠟⠋⣱⡟⢿⣿⡿⣷⡄⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣶⣶⣤⣌⡀⠀⢰⣿⡄⠈⠻⡇⠹⣿⣷⣤⡀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⢸⣿⣿⡟⢋⣥⠶⢋⣡⣤⣼⣿⣿⣿⣿⣷⣝⠳⣾⣿⣷⣄⠀⠁⠀⢻⣿⣿⣿⣄⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⣼⣿⠟⣠⣿⣷⣿⣿⣿⣿⣿⣿⣷⣶⣤⣀⣉⡁⠈⠻⣿⣿⣷⣄⠀⠘⠿⢿⣿⣿⣧⡀⠀                                        ';
+'                                         ⠀⠀⢀⣤⣾⣫⣶⣿⣿⣿⡿⠿⠿⠛⠿⠿⣿⣿⣿⣿⣿⣷⣬⡙⠲⢬⣿⣿⣿⣷⡀⠀⠀⠈⠙⢿⣷⡀                                        ';
+'                                         ⣰⣿⣿⠟⣿⣿⠟⣫⠥⠒⣊⣑⠒⠒⠶⠶⠭⣿⣿⣿⣿⣿⣿⣶⣄⠹⣿⣍⠛⠿⣆⠀⠀⠀⠀⠈⢣⠀                                        ';
+'                                         ⠘⣿⣧⣾⡫⣖⣯⣷⣾⣭⣤⣀⠀⠀⣴⣶⣶⣶⣾⡿⠿⣿⣿⣿⣿⣷⣽⣿⣷⡀⠈⠃⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠈⠻⢿⣿⢿⣿⠯⢡⣦⣶⣿⢿⣶⢬⡙⠿⣿⣿⣿⣷⣦⣌⡉⠛⢿⣿⣿⣿⣿⡄⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠘⢿⠀⠻⡇⠀⠀⠙⢿⡜⣿⠘⣿⣦⡈⢻⣿⣿⣿⣿⣿⣦⣄⠈⠻⣿⣿⣷⡀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠱⡹⣧⡙⢿⣿⡀⣿⣿⡛⠿⡷⣮⡙⠳⡀⠈⢻⣿⡇⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⡘⣿⣾⣿⡇⢹⣿⣿⡀⠈⠊⠻⣆⠀⠀⠀⢻⡗⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢳⡿⢻⣿⡇⢸⣿⣿⣧⢨⣷⣄⢹⣧⠀⠀⠀⠇⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠲⢤⣮⣁⣸⣿⡇⣼⣿⣿⡟⣼⣿⣿⣾⡟⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠛⠒⠛⠉⢹⡿⣿⠟⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'                                         ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠃⢸⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀                                        ';
+'⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⡀⠀⠀⣶⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀';
+'⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⣴⣿⣿⣿⡿⠁⠀⠀⢿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀';
+'⠀⠀⠀⠀⠀⢰⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⢀⣴⣿⣿⣿⠿⠀⠀⠀⠀⠀⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀';
+'⠀⠀⠀⠀⠀⣾⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⣀⡀⠀⢀⣀⣀⣀⡀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⢀⣀⣀⣀⣀⡀⠀⠀⠀⠀⠀⠀⣀⣀⣀⣀⣀⠀⠀⠀⣿⣿⣿⣿⡆⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⠟⠀⠀⠀⣀⣀⣀⣀⣀⣀⡀⠀⢀⣀⣀⣀⡀⠀⣀⣀⣀⣀⣀⣀⡀⠀⠀⠀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀';
+'⠀⠀⠀⠀⢰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿⣧⣶⣿⣿⣿⣿⣿⣿⣿⣷⡆⠀⠀⠀⢹⣿⣿⣿⣧⡀⠀⠀⠀⣀⣾⣿⣿⣿⠟⠁⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⢀⣾⣿⣿⣿⠏⠀⠀⠀⢀⣿⣿⣿⣿⣿⣿⠁⠀⢸⣿⣿⣿⣷⣾⣿⣿⣿⣿⣿⣿⣿⣶⣴⣿⣿⣿⣿⣿⣿⣿⣷⣆⠀⠀⠀';
+'⠀⠀⠀⠀⢸⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⢹⣿⣿⣿⡇⠀⠀⢸⣿⣿⣿⣿⠿⠛⠉⠉⠉⠻⣿⣿⣿⣿⡀⠀⠀⠀⠹⣿⣿⣿⣷⡀⢀⣴⣿⣿⣿⠿⠉⠀⠀⠀⠀⠀⠸⣿⣿⣿⣷⠀⠀⠀⢀⣾⣿⣿⣿⠏⠀⠀⠀⠀⠈⠉⢹⣿⣿⣿⡇⠀⠀⢸⣿⣿⣿⣿⠿⠋⠉⠉⠙⢿⣿⣿⣿⣿⠿⠋⠉⠉⠻⣿⣿⣿⣿⡆⠀⠀';
+'⠀⠀⠀⢀⣿⣿⣿⣿⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠇⠀⠀⢸⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠃⠀⠀⠀⠀⠹⣿⣿⣿⣷⣿⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⣿⡇⠀⢠⣾⣿⣿⡿⠋⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠇⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⣼⣿⣿⣿⠃⠀⠀⠀⠀⠀⣿⣿⣿⣿⠃⠀⠀';
+'⠀⠀⠀⢸⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⡿⠀⠀⠀⣾⣿⣿⣿⠁⠀⠀⠀⠀⠀⢰⣿⣿⣿⡟⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⠟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣇⢠⣾⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⡿⠀⠀⠀⣼⣿⣿⣿⠃⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⢰⣿⣿⣿⡏⠀⠀⠀';
+'⠀⠀⠀⣸⣿⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⢰⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣿⣿⣿⣿⣾⣿⣿⡿⠁⠀⠀⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⠃⠀⠀⢰⣿⣿⣿⡏⠀⠀⠀⠀⠀⢰⣿⣿⣿⡏⠀⠀⠀⠀⠀⣾⣿⣿⣿⡇⠀⠀⠀';
+'⠀⠀⢀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣼⣿⣿⣿⠁⠀⠀⢸⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠁⠀⠀⠀⢀⣴⣿⣿⣿⡿⢿⣿⣿⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⣾⣿⣿⣿⠁⠀⠀⠀⠀⠀⣿⣿⣿⣿⠁⠀⠀⠀';
+'⠀⠀⢸⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣶⣶⣶⣶⠆⠀⢰⣿⣿⣿⡏⠀⠀⠀⣿⣿⣿⣿⠀⠀⠀⠀⠀⠀⣸⣿⣿⣿⡏⠀⠀⢀⣰⣿⣿⣿⡿⠏⠀⠈⢿⣿⣿⣿⣶⠀⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⣿⣿⡟⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣿⣿⣿⡏⠀⠀⠀⣾⣿⣿⣿⠁⠀⠀⠀⠀⢰⣿⣿⣿⣿⠀⠀⠀⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀';
+'⠀⠀⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠀⠀⢸⣿⣿⣿⡇⠀⠀⢸⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⣠⣾⣿⣿⣿⠟⠁⠀⠀⠀⠀⢿⣿⣿⣿⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣿⡏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⠁⠀⠀⢸⣿⣿⣿⡇⠀⠀⠀⠀⠀⣸⣿⣿⣿⡇⠀⠀⠀⠀⠀⣿⣿⣿⣿⠇⠀⠀⠀⠀';
+'⠀⠀⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠁⠀⠀⠈⠉⠉⠉⠁⠀⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠈⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀⠀⠉⠉⠉⠉⠀⠀⠀⠀⠀';
+'⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀';
+}
+
+lvim.builtin.alpha.dashboard.section.footer.val = {"Personal IDE configuration of pedroesmerio"}
 
 -- if you don't want all the parsers change this to a table of the ones you want
 lvim.builtin.treesitter.ensure_installed = {
   "bash",
   "c",
-  "html",
   "javascript",
   "json",
   "lua",
@@ -111,13 +147,17 @@ lvim.builtin.treesitter.highlight.enabled = true
 -- ---@usage disable automatic installation of servers
 -- lvim.lsp.automatic_servers_installation = false
 
--- ---@usage Select which servers should be configured manually. Requires `:LvimCacheReset` to take effect.
--- See the full default list `:lua print(vim.inspect(lvim.lsp.override))`
--- vim.list_extend(lvim.lsp.override, { "pyright" })
-
--- ---@usage setup a server -- see: https://www.lunarvim.org/languages/#overriding-the-default-configuration
+-- ---configure a server manually. !!Requires `:LvimCacheReset` to take effect!!
+-- ---see the full default list `:lua print(vim.inspect(lvim.lsp.automatic_configuration.skipped_servers))`
+-- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "pyright" })
 -- local opts = {} -- check the lspconfig documentation for a list of all possible options
--- require("lvim.lsp.manager").setup("pylsp", opts)
+-- require("lvim.lsp.manager").setup("pyright", opts)
+
+-- ---remove a server from the skipped list, e.g. eslint, or emmet_ls. !!Requires `:LvimCacheReset` to take effect!!
+-- ---`:LvimInfo` lists which server(s) are skiipped for the current filetype
+-- vim.tbl_map(function(server)
+--   return server ~= "emmet_ls"
+-- end, lvim.lsp.automatic_configuration.skipped_servers)
 
 -- -- you can set a custom on_attach function that will be used for all the language servers
 -- -- See <https://github.com/neovim/nvim-lspconfig#keybindings-and-completion>
@@ -163,8 +203,8 @@ lvim.builtin.treesitter.highlight.enabled = true
 --   },
 -- }
 
+-- Additional Plugins
 lvim.plugins = {
-  -- Additional Plugins
   {"lunarvim/colorschemes"},
   {"folke/tokyonight.nvim"},
   {"dracula/vim"},
@@ -172,10 +212,10 @@ lvim.plugins = {
   {"pantharshit00/vim-prisma"},
   {"styled-components/vim-styled-components"},
   {"github/copilot.vim"},
---   {
---     "folke/trouble.nvim",
---     cmd = "TroubleToggle",
---   },
+  {
+    "folke/trouble.nvim",
+    cmd = "TroubleToggle",
+  },
 }
 
 -- Autocommands (https://neovim.io/doc/user/autocmd.html)
